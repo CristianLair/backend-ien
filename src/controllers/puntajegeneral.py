de52de96f6782cee1f75413a0ff47a355c95d178
Luccasossa <luccasosaa12@gmail.com>
2026-06-15 01:24:10 -0300

    Api nueva

--- src/controllers/puntajegeneral.py
+++ src/controllers/puntajegeneral.py
@@ -1,33 +1,15 @@
-ranking_selecciones = {
-    "Canadá": 0, "México": 0, "Estados Unidos": 0,
-    "Alemania": 0, "Austria": 0, "Bélgica": 0,
-    "Bosnia y Herzegovina": 0, "Croacia": 0, "Escocia": 0,
-    "España": 0, "Francia": 0, "Inglaterra": 0,
-    "Noruega": 0, "Países Bajos": 0, "Portugal": 0,
-    "República Checa": 0, "Suecia": 0, "Suiza": 0,
-    "Turquía": 0, "Argentina": 0, "Brasil": 0,
-    "Colombia": 0, "Ecuador": 0, "Paraguay": 0,
-    "Uruguay": 0, "Argelia": 0, "Cabo Verde": 0,
-    "Costa de Marfil": 0, "Egipto": 0, "Ghana": 0,
-    "Marruecos": 0, "RD Congo": 0, "Senegal": 0,
-    "Sudáfrica": 0, "Túnez": 0, "Arabia Saudita": 0,
-    "Australia": 0, "Corea del Sur": 0, "Irak": 0,
-    "Irán": 0, "Japón": 0, "Jordania": 0,
-    "Qatar": 0, "Uzbekistán": 0, "Curazao": 0,
-    "Haití": 0, "Panamá": 0, "Nueva Zelanda": 0
-}
+from flask import Flask, jsonify
+from pymongo import MongoClient
+from bson import ObjectId
 
-def cargar_puntos(pais, puntos):
-    if pais not in ranking_selecciones:
-        return None, f"El país '{pais}' no existe en el ranking"
+cliente = MongoClient("mongodb://localhost:27017/")
+db = cliente["prode-ien"]
+coleccion = db["paises"]
 
-    ranking_selecciones[pais] = int(puntos)
+def obtener_paises():
+    documento = coleccion.find_one({}, {"_id": 0})  # excluye _id
 
-    tabla = sorted(
-        ranking_selecciones.items(),
-        key=lambda x: x[1],
-        reverse=True
-    )
+    if documento:
+        return documento, None
 
-    return [{"posicion": i, "pais": p, "puntos": pts}
-            for i, (p, pts) in enumerate(tabla, start=1)], None  
+    return None, "No hay datos"
--- src/rest/indesx.py
+++ src/rest/indesx.py
@@ -7,30 +7,21 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from db.db import db
-from controllers.puntajegeneral import cargar_puntos
+from controllers.puntajegeneral import obtener_paises
 app = Flask(__name__)
 CORS(app)
 @app.route("/")
 def home():
     return {"mensaje": "Servidor funcionando ✅"}
 
-@app.route("/ranking", methods=["POST"])
+@app.route("/paises", methods=["GET"])
 def ranking():
-    body = request.get_json()
-
-    pais = body.get("pais")
-    puntos = body.get("puntos")
-
-    if pais is None or puntos is None:
-        return jsonify({"error": "Faltan los campos 'pais' y 'puntos'"}), 400
-
-    tabla, error = cargar_puntos(pais, puntos)
+    tabla, error = obtener_paises()
 
     if error:
         return jsonify({"error": error}), 404
 
     return jsonify(tabla), 200
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
     app.run(debug=False, host="0.0.0.0", port=port)
